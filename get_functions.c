81f11b6bfe2e2ae662a1b79adf1e38a509502f8b
otalorajuand <otalorajuand@gmail.com>
2022-07-20 14:23:34 -0700

    Addition of get functions file and the format_functions file

---
+++ get_functions.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * get_op_func - Selects the correct function to perform
+ * the format asked by the user.
+ *
+ * @s: The format passed as argument.
+ *
+ * Return: A pointer to the function corresponding
+ * to the format given as a parameter.
+ */
+char* (*get_op_func(char s))(va_list)
+{
+	op_t func[] = {
+		{"c", func_char},
+		{NULL, NULL},
+	};
+
+	int i = 0;
+
+	while (func[i].format != NULL && *(func[i].format) != s)
+		i++;
+
+	return (func[i].f);
+}
---
+++ main.h
@@ -0,0 +1,38 @@
+#ifndef PRINTF_H
+#define PRINTF_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+
+/**
+ * struct op - Struct op
+ *
+ * @counter: The counter
+ * @buffer: The buffer to save the string.
+ */
+
+typedef struct op
+{
+	int counter;
+	char buffer[1024];
+} string_to_print;
+
+/**
+ * struct formats - Struct formats
+ *
+ * @format: The character that specify the format.
+ * @f: The function that expands a specfic format.
+ */
+
+typedef struct formats
+{
+	char *format;
+	char *(*f)(va_list ap);
+} op_t;
+
+int _printf(const char *format, ...);
+char *func_char(va_list ap);
+char *(*get_op_func(char s))(va_list);
+
+#endif /* PRINTF_H */
