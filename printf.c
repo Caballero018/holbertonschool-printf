1e992fbc9fac72484f8c8af3d5ccd8a51e65f5ee
Caballero018 <alejoxcaballero18@gmail.com.com>
2022-07-20 14:10:44 -0700

    Function that produces output according to a format.

--- printf.c
+++ printf.c
@@ -4,7 +4,7 @@
 #include <stdarg.h>
 
 /**
- * _prinf - Function that produces output
+ * _printf - Function that produces output
  * according to a format.
  *
  * @format: String to be formatted.
@@ -20,14 +20,13 @@ int _printf(const char *format, ...)
 	va_list ap;
 
 	va_start(ap, format);
-
 	stringf.counter = 0;
 
 	while (format[i] != 0)
 	{
 		if (format[i] == '%')
 		{
-			ptr = get_op_func(format[i+1]);
+			ptr = get_op_func(format[i + 1]);
 			res = ptr(ap);
 			stringf.buffer[stringf.counter] = res[0];
 			stringf.counter++;
@@ -41,5 +40,5 @@ int _printf(const char *format, ...)
 		i++;
 	}
 	write(1, stringf.buffer, stringf.counter);
-	return(stringf.counter);
+	return (stringf.counter);
 }
