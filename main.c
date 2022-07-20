97ea0d0148d7689d02b9247c50d05d190a07759f
Caballero018 <alejoxcaballero18@gmail.com.com>
2022-07-20 14:36:42 -0700

    Test of the function

---
+++ main.c
@@ -0,0 +1,23 @@
+#include <limits.h>
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+    int len;
+    int len2;
+    char ws = 's';
+
+    len = _printf("Let's try to printf  %c a simple sentence.\n", ws);
+    len2 = printf("Let's try to printf  %c a simple sentence.\n", ws);
+
+    printf("%d\n", len);
+    printf("%d\n", len2);
+
+    return (0);
+}
